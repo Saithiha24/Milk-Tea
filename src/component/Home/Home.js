7ab19e84bae592662862852e31e06bdcacb7f167
Sai <sai081118@gmail.comgit commit>
2022-01-17 17:05:24 +0630

    foundation of milk-tea website

---
+++ src/component/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Delivery from "./Delivery";
+import PopularItems from "./PopularItems";
+
+const Home = () => {
+  return (
+    <div>
+      <PopularItems />
+      <Delivery />
+    </div>
+  );
+};
+
+export default Home;
