81432c163115e4fc436a6203f5d5094b7859fe2f
Sai <sai081118@gmail.comgit commit>
2022-01-18 18:04:58 +0630

    finished footer

--- src/UI/Footer.js
+++ src/UI/Footer.js
@@ -39,7 +39,7 @@ const Footer = () => {
             </li>
           </ul>
         </Col>
-        <Col className="text-align-center" col={4}>
+        <Col className="text-align-center" xs={12} lg={6}>
           <iframe
             title="shop-location"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3819.5667998290924!2d96.18521291437493!3d16.798214723951695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c1ed3e32ee3e47%3A0x47b9e7dd7623c8bd!2sSHO%20Drinks!5e0!3m2!1smy!2smm!4v1642497710893!5m2!1smy!2smm"
