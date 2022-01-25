84b89b064b8641231b9fd9ae05bdab2eadae566d
Kyaw Sitt Thway <kst@gmail.com>
2022-01-25 16:42:59 +0630

    Add ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./component/Home/Home";
 import Product from "./component/Product/Product";
 import DeskNavbar from "./UI/DeskNavbar";
 import MobileNavbar from "./UI/MobileNavbar";
+import ScrollButton from "./UI/ScrollToTop";
 const App = () => {
   return (
     <Router>
@@ -15,6 +16,7 @@ const App = () => {
         <Route exact path="/delivery" element={<Delivery />} />
         <Route exact path="/products" element={<Product />} />
       </Routes>
+      <ScrollButton/>
       <MobileNavbar/>
     </Router>
   );
